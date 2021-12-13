21b0f9b353ca0fc5e6c7a7d53bb0ec3ba92c78f3
Koune <ioannis.koune@tno.nl>
2021-12-13 15:38:48 +0100

    feat: Added additive 1D cholesky loglikelihood example

--- test/integration_test/test_loglikelihood_1D_cholesky.py
+++ test/integration_test/test_loglikelihood_1D_cholesky.py
@@ -12,17 +12,21 @@ from tripy.kernels import Exponential
 from tripy.loglikelihood import chol_loglike_1D
 
 
-def test_chol_loglike_1D():
+def test_chol_loglike_1D_multiplicative():
     # ============================================================================
-    # SETUP
+    # Multiplicative model prediction error. The covariance matrix is composed
+    # of a correlated model prediction error scaled by the model output, plus
+    # an additive diagonal noise component.
     #
     # * Observations on unevenly spaced grid
     # * Noise std. dev. is a random vector
+    # * Model std. dev. acts as a coefficient of variation, as the correlation
+    # matrix is scaled by the model prediction.
     # ============================================================================
     N = 100
     lcorr = 10.0
     std_model = np.repeat(2.0, N)
-    std_meas = np.random.rand(N) + 0.1  # np.repeat(2.0, N)
+    std_meas = np.random.rand(N) + 0.1
 
     # Coord vector and a test function
     x = np.sort(np.linspace(0, 1, N) + 0.1 * np.random.rand(N))
@@ -31,15 +35,17 @@ def test_chol_loglike_1D():
     def phys_model_func(x):
         return np.cos(x) + 5
 
-    # Synthetic observations
+    # Model prediction
     y_model = phys_model_func(x)
-    v_obs = np.random.rand(N) + y_model
+
+    # Vector of residuals between measurement and prediction
+    y_res = np.random.rand(N)
 
     # ============================================================================
     # Compare 1D Cholesky with reference solution
     # ============================================================================
 
-    # Assemble the covariance matrix
+    # Assemble the covariance matrix for the reference solution
     e_cov_mx = np.diag(np.ones(N) * std_meas ** 2)
     kernel = Exponential(np.reshape(x, (-1, 1)), np.ones(N), length_scale=lcorr)
     corr_mx = kernel.eval(std_model, length_scale=lcorr)
@@ -47,8 +53,54 @@ def test_chol_loglike_1D():
     cov_mx = kph_cov_mx + e_cov_mx
 
     # Evaluate the loglikelihood
-    logL_ref = stats.multivariate_normal.logpdf(v_obs, cov=cov_mx)
-    logL_chol = chol_loglike_1D(x, y_model, v_obs, lcorr, std_model, std_meas)
+    logL_ref = stats.multivariate_normal.logpdf(y_res, cov=cov_mx)
+    logL_chol = chol_loglike_1D(x, y_model, y_res, lcorr, std_model, std_meas)
+
+    # Compare the two solutions
+    print(logL_ref)
+    print(logL_chol)
+    assert np.allclose(logL_ref, logL_chol)
+
+
+def test_chol_loglike_1D_additive():
+    # ============================================================================
+    # Additive model prediction error. The covariance is composed of a
+    # correlated model prediction uncertainty defined by the std. dev.
+    # plus an additive diagonal noise component.
+    #
+    # * Observations on unevenly spaced grid
+    # * Noise std. dev. is a random vector
+    # * Model std. dev. is the standard deviation of the additive model
+    # prediction uncerainty.
+    # ============================================================================
+    N = 100
+    lcorr = 10.0
+    std_model = np.repeat(2.0, N)
+    std_meas = np.random.rand(N) + 0.1
+
+    # Coord vector and a test function
+    x = np.sort(np.linspace(0, 1, N) + 0.1 * np.random.rand(N))
+
+    # In the case of additive model prediction uncertainty, a vector of ones
+    # is given as input to chol_loglike_1D instead of model predictions
+    y_model = np.ones(N)
+
+    # Vector of residuals between measurement and prediction
+    y_res = np.random.rand(N)
+
+    # ============================================================================
+    # Compare 1D Cholesky with reference solution
+    # ============================================================================
+
+    # Assemble the covariance matrix for the reference solution
+    e_cov_mx = np.diag(np.ones(N) * std_meas ** 2)
+    kernel = Exponential(np.reshape(x, (-1, 1)), np.ones(N), length_scale=lcorr)
+    corr_mx = kernel.eval(std_model, length_scale=lcorr)
+    cov_mx = corr_mx + e_cov_mx
+
+    # Evaluate the loglikelihood
+    logL_ref = stats.multivariate_normal.logpdf(y_res, cov=cov_mx)
+    logL_chol = chol_loglike_1D(x, y_model, y_res, lcorr, std_model, std_meas)
 
     # Compare the two solutions
     print(logL_ref)
