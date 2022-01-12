2bdeab13971589db9ceff2c7c2c603533c80d7a8
Koune <ioannis.koune@tno.nl>
2022-01-12 13:07:18 +0100

    feat: Updated integration test for cholesky 1D likelihood

--- test/integration_test/test_cholesky_loglikelihood_1D.py
+++ test/integration_test/test_cholesky_loglikelihood_1D.py
@@ -54,7 +54,9 @@ def test_chol_loglike_1D_multiplicative():
 
     # Evaluate the loglikelihood
     logL_ref = stats.multivariate_normal.logpdf(y_res, cov=cov_mx)
-    logL_chol = chol_loglike_1D(x, y_model, y_res, lcorr, std_model, std_meas)
+    logL_chol = chol_loglike_1D(
+        x, y_res, lcorr, std_model, std_meas=std_meas, y_model=y_model
+    )
 
     # Compare the two solutions
     print(logL_ref)
@@ -100,7 +102,87 @@ def test_chol_loglike_1D_additive():
 
     # Evaluate the loglikelihood
     logL_ref = stats.multivariate_normal.logpdf(y_res, cov=cov_mx)
-    logL_chol = chol_loglike_1D(x, y_model, y_res, lcorr, std_model, std_meas)
+    logL_chol = chol_loglike_1D(
+        x, y_res, lcorr, std_model, std_meas=std_meas, y_model=y_model
+    )
+
+    # Compare the two solutions
+    print(logL_ref)
+    print(logL_chol)
+    assert np.allclose(logL_ref, logL_chol)
+
+
+def test_chol_loglike_1D_no_noise():
+    # ============================================================================
+    # Multiplicative, no measurement uncertainty
+    #
+    # * Observations on unevenly spaced grid
+    # * Model std. dev. is the standard deviation of the additive model
+    # prediction uncerainty.
+    # ============================================================================
+    N = 100
+    lcorr = 10.0
+    std_model = np.repeat(2.0, N)
+
+    # Coord vector and a test function
+    x = np.sort(np.linspace(0, 1, N) + 0.1 * np.random.rand(N))
+
+    # Example model
+    def phys_model_func(x):
+        return np.cos(x) + 5
+
+    # Model prediction
+    y_model = phys_model_func(x)
+
+    # Vector of residuals between measurement and prediction
+    y_res = np.random.rand(N)
+
+    # ============================================================================
+    # Compare 1D Cholesky with reference solution
+    # ============================================================================
+
+    # Assemble the covariance matrix for the reference solution
+    kernel = Exponential(np.reshape(x, (-1, 1)), np.ones(N), length_scale=lcorr)
+    corr_mx = kernel.eval(std_model, length_scale=lcorr)
+    kph_cov_mx = np.matmul(np.diag(y_model), np.matmul(corr_mx, np.diag(y_model)))
+
+    # Evaluate the loglikelihood
+    logL_ref = stats.multivariate_normal.logpdf(y_res, cov=kph_cov_mx)
+    logL_chol = chol_loglike_1D(x, y_res, lcorr, std_model, y_model=y_model)
+
+    # Compare the two solutions
+    print(logL_ref)
+    print(logL_chol)
+    assert np.allclose(logL_ref, logL_chol)
+
+    # ============================================================================
+    # Additive, no measurement uncertainty
+    #
+    # * Observations on unevenly spaced grid
+    # * Model std. dev. is the standard deviation of the additive model
+    # prediction uncerainty.
+    # ============================================================================
+    N = 100
+    lcorr = 10.0
+    std_model = np.repeat(2.0, N)
+
+    # Coord vector and a test function
+    x = np.sort(np.linspace(0, 1, N) + 0.1 * np.random.rand(N))
+
+    # Vector of residuals between measurement and prediction
+    y_res = np.random.rand(N)
+
+    # ============================================================================
+    # Compare 1D Cholesky with reference solution
+    # ============================================================================
+
+    # Assemble the covariance matrix for the reference solution
+    kernel = Exponential(np.reshape(x, (-1, 1)), np.ones(N), length_scale=lcorr)
+    corr_mx = kernel.eval(std_model, length_scale=lcorr)
+
+    # Evaluate the loglikelihood
+    logL_ref = stats.multivariate_normal.logpdf(y_res, cov=corr_mx)
+    logL_chol = chol_loglike_1D(x, y_res, lcorr, std_model)
 
     # Compare the two solutions
     print(logL_ref)
