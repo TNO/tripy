831484f2447c10f2784453b86d921a760f501e76
Koune <ioannis.koune@tno.nl>
2022-02-03 13:09:25 +0100

    test: Updated test_block_cholesky_decomposition_2D to avoid sometimes failing due to inversion of full correlation matrix

--- test/integration_test/test_block_cholesky_decomposition_2D.py
+++ test/integration_test/test_block_cholesky_decomposition_2D.py
@@ -3,6 +3,12 @@ Testing for the ND Cholesky loglikelihood with vector noise and associated funct
 
     * symm_tri_block_chol: Block Cholesky decomposition of block symmetric
     tridiagonal matrices
+
+Notes:
+    * For values of Nx >~ 50, this test will sometimes fail. This is likely
+    due to numerical inaccuracy in the inversion of the correlation matrix.
+    Repeating the test with a tridiagonal inverse calculated analytically,
+    does not lead to errors, tested for Nx = 500.
 """
 
 # ============================================================================
@@ -20,12 +26,12 @@ def test_block_cholesky_decomposition():
     # =====================================================================
 
     # Parameters
-    Nx = 50
-    Nt = 5
+    Nx = 10
+    Nt = 10
     x = np.sort(np.linspace(0, 1, Nx) + np.random.rand(Nx) * 0.1)
     t = np.sort(np.linspace(0, 1, Nt) + np.random.rand(Nt) * 0.1)
 
-    std_meas = np.tile(1.0, (Nx, Nt)) + np.random.rand(Nx, Nt) + 0.1
+    std_meas = np.tile(0.01, (Nx, Nt)) + np.random.rand(Nx, Nt)  # + 0.1
     std_model = np.random.rand(Nt) + 0.1
 
     lcorr_x = 1.0
@@ -41,6 +47,17 @@ def test_block_cholesky_decomposition():
     cov_mx_t = kernel_t.eval(std_model)
     inv_cov_mx_x = np.linalg.inv(cov_mx_x)
     inv_cov_mx_t = np.linalg.inv(cov_mx_t)
+
+    # test_x = inv_cov_vec_1D(x, lcorr_x, 1.0)
+    # test_t = inv_cov_vec_1D(t, lcorr_t, std_model)
+    # inv_cov_x_u = np.diag(test_x[1], k=1)
+    # inv_cov_x_l = np.diag(test_x[1], k=-1)
+    # inv_cov_mx_x = inv_cov_x_u + inv_cov_x_l
+    # np.fill_diagonal(inv_cov_mx_x, test_x[0])
+    # inv_cov_t_u = np.diag(test_t[1], k=1)
+    # inv_cov_t_l = np.diag(test_t[1], k=-1)
+    # inv_cov_mx_t = inv_cov_t_u + inv_cov_t_l
+    # np.fill_diagonal(inv_cov_mx_t, test_t[0])
 
     # Get inverse cov vector in space and time
     Cx_0 = np.diag(inv_cov_mx_x)
@@ -147,4 +164,10 @@ def test_block_cholesky_decomposition():
         assert np.allclose(blck_C_ref, C_test[i])
 
 
-test_block_cholesky_decomposition()
+# Ntry = 1000
+# for i in range(Ntry):
+#     print(i)
+#     x, t, inv_x, inv_t, blk_ref, blk_test, flag = test_block_cholesky_decomposition()
+#     if flag is not None:
+#         print(f"Failed with flag = {flag}")
+#         break
